38748c6abc6f16fa27c5e9d44457c926986b82d9
Mahbub <prof.bub@live.com>
2019-12-18 00:02:35 +0700

    export IResponse

--- src/response.ts
+++ src/response.ts
@@ -1,22 +1,22 @@
-interface IPageInfo {
+export interface IPageInfo {
   nextCursor: string | null;
   prevCursor: string | null;
   hasNextPage: boolean;
   hasPrevPage: boolean;
 }
 
-interface IResponse {
+export interface IResponse {
   edges: any;
   message: string;
   pageInfo: IPageInfo;
   totalCount?: number;
 }
 
-export default (arg: any, options = { status: "", message: "", totalCount: 0, limit: 10, offset: 0 }): IResponse => {
+export default (arg: any, options = {status: "", message: "", totalCount: 0, limit: 10, offset: 0}): IResponse => {
   const data = (arg.data ? (arg.data.data ? arg.data.data : arg.data) : arg);
   const edges = data.edges || data;
   const statusCode = +options.status || +data.status || +data.statusCode || 200;
-  const total = (+options.totalCount) || +(edges instanceof Array || edges instanceof String ? edges.length : 0)
+  const total = (+options.totalCount) || +((edges instanceof Array || edges instanceof String) ? edges.length : 0)
     || (edges instanceof Object ? Object.keys(edges).length : 0);
   const totalPage = Math.ceil(total / options.limit) || 1;
   const current = Math.ceil(options.offset / options.limit) + 1;
@@ -24,8 +24,8 @@ export default (arg: any, options = { status: "", message: "", totalCount: 0, li
   const hasPrevPage = current > 1;
   return {
     edges,
-    message   : options.message || data.message || +statusCode < 400 ? "success" : "Internal Server Error",
-    pageInfo  : data.pageInfo || {
+    message: options.message || data.message || +statusCode < 400 ? "success" : "Internal Server Error",
+    pageInfo: data.pageInfo || {
       hasNextPage,
       hasPrevPage,
       nextCursor: hasNextPage ? current + 1 + "" : "",
